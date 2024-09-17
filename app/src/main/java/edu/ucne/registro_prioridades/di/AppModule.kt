3f82b39bc3cbd88dd43607af8edefcf7c9b86ecb
SDuran-19 <samuelduran1919@gmail.com>
2024-09-17 17:48:17 -0400

    agregando appmodule y converters

---
+++ app/src/main/java/edu/ucne/registro_prioridades/di/AppModule.kt
@@ -0,0 +1,33 @@
+package edu.ucne.registro_prioridades.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.registro_prioridades.local.database.PrioridadDb
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+    @Provides
+    @Singleton
+    fun providePrioridadDb(@ApplicationContext appContext: Context) =
+        Room.databaseBuilder(
+            appContext,
+            PrioridadDb::class.java,
+            "Prioridad.db"
+        ).fallbackToDestructiveMigration()
+            .build()
+
+    @Provides
+    @Singleton
+    fun providePrioridadDao(prioridadDb: PrioridadDb) = prioridadDb.prioridadDao()
+
+    @Provides
+    @Singleton
+    fun provideTicketDao(prioridadDb: PrioridadDb) = prioridadDb.ticketDao()
+}
