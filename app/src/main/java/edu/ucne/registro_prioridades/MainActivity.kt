988d933fb1ef78630ffd07aabf971e9c2d234bed
SDuran-19 <samuelduran1919@gmail.com>
2024-09-17 17:48:42 -0400

    modificando MainActivity

--- app/src/main/java/edu/ucne/registro_prioridades/MainActivity.kt
+++ app/src/main/java/edu/ucne/registro_prioridades/MainActivity.kt
@@ -3,79 +3,41 @@ package edu.ucne.registro_prioridades
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Refresh
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import androidx.room.Room
-import edu.ucne.registro_prioridades.local.database.PrioridadDb
+import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.registro_prioridades.local.entities.PrioridadEntity
-import edu.ucne.registro_prioridades.presentation.navigation.prioridad.PrioridadScreen
-import edu.ucne.registro_prioridades.presentation.navigation.registro_prioridadesNavHost
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import edu.ucne.registro_prioridades.presentation.navigation.PrioridadNavHost
+import edu.ucne.registro_prioridades.ui.theme.PrioridadesTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-    private lateinit var prioridadDb: PrioridadDb
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        prioridadDb = Room.databaseBuilder(
-            applicationContext,
-            PrioridadDb::class.java,
-            "Prioridad.db"
-        ).fallbackToDestructiveMigration().build()
-
+        enableEdgeToEdge()
         setContent {
-            Registro_PrioridadesTheme {
+            PrioridadesTheme {
                 val navHost = rememberNavController()
-                registro_prioridadesNavHost(prioridadDb, navHost)
+                PrioridadNavHost(navHost)
             }
         }
     }
 
+    @Preview(showBackground = true, showSystemUi = true)
+    @Composable
+    fun PrioridadScreenPreview() {
+        var prioridadList: List<PrioridadEntity> = listOf(
+            PrioridadEntity(1, "Alta", 10),
+            PrioridadEntity(2, "Media", 5),
+            PrioridadEntity(3, "Baja", 1)
+        )
+        PrioridadesTheme {
 
+        }
+    }
 }
 
-@Composable
-fun Registro_PrioridadesTheme(content: @Composable () -> Unit) {
-    MaterialTheme(
-        content = content
-    )
-}
